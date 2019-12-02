785495735a9d056dcd4bf3251f0b56e97e1d840c
isidro1108 <euris110811@gmail.com>
2019-12-02 10:49:46 -0400

    Se ha eliminado el atributo de instancia name
    Ademas los atributos de clase, colors y pieces, ahora estan
    fuera de la clase y comentados

--- pieces.py
+++ pieces.py
@@ -1,10 +1,9 @@
-class Piece:
-    colors = {'white': 0, 'black': 1}
-    pieces = {'pawn': ['♟','♙', 1], 'tower': ['♜','♖', 5], 'horse': ['♞','♘', 3], 
-             'bishop': ['♝','♗', 3], 'queen': ['♛','♕', 9], 'king': ['♚','♔', 'abstract value']}
+# colors = {'white': 0, 'black': 1}
+# pieces = {'pawn': ['♟','♙', 1], 'tower': ['♜','♖', 5], 'horse': ['♞','♘', 3], 
+#           'bishop': ['♝','♗', 3], 'queen': ['♛','♕', 9], 'king': ['♚','♔', 'abstract value']}
 
-    def __init__(self, name, color, position):
-        self.name = name
+class Piece:
+    def __init__(self, color, position):
         self.color = color
         self.position = position
         self.value = self.pieces[self.name][2]
