66864eeec38a74dbbb33eaa2b2370f52001941dd
pRodriguez1630 <pRodriguez1630@gmail.com>
2019-12-03 11:03:44 -0400

    Se le dio todo los posibles moviemientos de la Reina

--- pieces.py
+++ pieces.py
@@ -23,6 +23,7 @@ class Queen(Piece):
     def __init__(self, color, p1, p2):
         Piece.__init__(self, color, p1, p2)
         self.symbol = self.symbols[self.color_p[color]]
+        self.moves = [(p1 + 1, p2 + 2), (p1 + 1, p2 - 1), (p1 - 1, p2 + 1), (p1, p2 + 1),(p1 + 1, p2), (p1 - 1, p2 - 2),(p1 - 1, p2),(p1 , p2 -1) ]
 
 class Bishop(Piece):
     symbols = ['♝','♗']
