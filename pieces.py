0a2aaef927139b88c203f1c585b1281cf1122ed8
Isidro Morla <euris110811@gmail.com>
2019-12-08 14:55:10 -0400

    Se han resumido todos las clases que heredan de Piece (el método move_piece)

--- pieces.py
+++ pieces.py
@@ -1,5 +1,3 @@
-from table import Table
-
 class Piece:
     color_p = {'white': 0, 'black': 1}
 
@@ -11,167 +9,120 @@ class Piece:
         self.is_defended = None
 
 class King(Piece):
-    symbols = ['♚','♔']
+    # symbols = ['♚','♔']
 
     def __init__(self, color, p1, p2):
         Piece.__init__(self, color, p1, p2)
-        self.symbol = self.symbols[self.color_p[color]]
+        # self.symbol = self.symbols[self.color_p[color]]
         self.in_check = False
         self.in_checkmate = False
-        self.movements = [(p1 + 1, p2 + 1), (p1 - 1, p2 - 1), (p1 + 1, p2 - 1), (p1 - 1, p2 + 1), 
-                        (p1 + 1, p2), (p1 - 1, p2), (p1, p2 + 1), (p1, p2 - 1)]
+        self.movements = [(1, 1), (-1, -1), (1, -1), (-1, 1),
+                        (1, 0), (-1, 0), (0, 1), (0, -1)]
     
-    def move_piece(self, table, p1, p2):
-        new_table = Table()
-        empty_table = new_table.create_table()
-        p_movements = []
-        for movement in self.movements:
-            if movement[0] < 8 and movement[0] >= 0:
-                if movement[1] < 8 and movement[1] >= 0:
-                    p_movements.append(movement)
-        if (p1, p2) in p_movements:
-            table[self.p1][self.p2] = empty_table[self.p1][self.p2]
-            table[p1][p2] = self.symbol
+    def move_piece(self, p1, p2):
+        if (p1 - self.p1, p2 - self.p2) in self.movements:
             self.p1 = p1
             self.p2 = p2
-            self.movements = [(p1 + 1, p2 + 1), (p1 - 1, p2 - 1), (p1 + 1, p2 - 1), (p1 - 1, p2 + 1), 
-                                (p1 + 1, p2), (p1 - 1, p2), (p1, p2 + 1), (p1, p2 - 1)]
             self.moves+= 1
         else:
-            print('El movimiento que ha insertado es invalido') 
+            print('El movimiento que ha insertado es invalido')
 
 class Queen(Piece):
-    symbols = ['♛','♕']
+    # symbols = ['♛','♕']
 
     def __init__(self, color, p1, p2):
         Piece.__init__(self, color, p1, p2)
-        self.symbol = self.symbols[self.color_p[color]]
-        self.movements = [(p1 + 1, p2 + 2), (p1 + 1, p2 - 1), (p1 - 1, p2 + 1), (p1, p2 + 1),
-                        (p1 + 1, p2), (p1 - 1, p2 - 2),(p1 - 1, p2),(p1 , p2 -1)]
+        # self.symbol = self.symbols[self.color_p[color]]
+        self.movements = [(1, 1), (-1, -1), (1, -1), (-1, 1),
+                        (1, 0), (-1, 0), (0, 1), (0, -1)]
 
-    def move_piece(self, table, p1, p2):
-        new_table = Table()
-        empty_table = new_table.create_table()
-        p_movements = []
-        for movement in self.movements:
-            if movement[0] < 8 and movement[0] >= 0:
-                if movement[1] < 8 and movement[1] >= 0:
-                    p_movements.append(movement)
-        if (p1, p2) in p_movements:
-            table[self.p1][self.p2] = empty_table[self.p1][self.p2]
-            table[p1][p2] = self.symbol
+    def move_piece(self, p1, p2):
+        if (p1 - self.p1, p2 - self.p2) in self.movements:
             self.p1 = p1
             self.p2 = p2
-            self.movements = [(p1 + 1, p2 + 1), (p1 - 1, p2 - 1), (p1 + 1, p2 - 1), (p1 - 1, p2 + 1), 
-                                (p1 + 1, p2), (p1 - 1, p2), (p1, p2 + 1), (p1, p2 - 1)]
             self.moves+= 1
         else:
             print('El movimiento que ha insertado es invalido')
 
 class Bishop(Piece):
-    symbols = ['♝','♗']
+    # symbols = ['♝','♗']
 
     def __init__(self, color, p1, p2):
         Piece.__init__(self, color, p1, p2)
-        self.symbol = self.symbols[self.color_p[color]]
-        self.movements = [(p1 - 1, p2 - 1), (p1 + 1, p2 + 1), (p1 - 1, p2 + 1), (p1 + 1, p2 - 1)]
+        # self.symbol = self.symbols[self.color_p[color]]
+        self.movements = [(-1, -1), (1, 1), (-1, 1), (1, -1)]
     
-    def move_piece(self, table, p1, p2):
-        new_table = Table()
-        empty_table = new_table.create_table()
-        p_movements = []
-        for movement in self.movements:
-            if movement[0] < 8 and movement[0] >= 0:
-                if movement[1] < 8 and movement[1] >= 0:
-                    p_movements.append(movement)
-        if (p1, p2) in p_movements:
-            table[self.p1][self.p2] = empty_table[self.p1][self.p2]
-            table[p1][p2] = self.symbol
+    def move_piece(self, p1, p2):
+        if (p1 - self.p1, p2 - self.p2) in self.movements:
             self.p1 = p1
             self.p2 = p2
-            self.movements = [(p1 - 1, p2 - 1), (p1 + 1, p2 + 1), (p1 - 1, p2 + 1), (p1 + 1, p2 - 1)]
             self.moves+= 1
         else:
             print('El movimiento que ha insertado es invalido')
 
 class Knight(Piece):
-    symbols = ['♞','♘']
+    # symbols = ['♞','♘']
 
     def __init__(self, color, p1, p2):
         Piece.__init__(self, color, p1, p2)
-        self.symbol = self.symbols[self.color_p[color]]
-        self.movements = [(p1 + 2, p2 + 1), (p1 - 2, p2 - 1), (p1 + 2, p2 - 1), (p1 - 2, p2 + 1),
-                        (p1 + 1, p2 + 2), (p1 - 1, p2 - 2), (p1 + 1, p2 - 2), (p1 - 1, p2 + 2)]
+        # self.symbol = self.symbols[self.color_p[color]]
+        self.movements = [(2, 1), (-2, -1), (2, -1), (-2, 1),
+                        (1, 2), (-1, -2), (1, -2), (-1, 2)]
     
-    def move_piece(self, table, p1, p2):
-        new_table = Table()
-        empty_table = new_table.create_table()
-        p_movements = []
-        for movement in self.movements:
-            if movement[0] < 8 and movement[0] >= 0:
-                if movement[1] < 8 and movement[1] >= 0:
-                    p_movements.append(movement)
-        if (p1, p2) in p_movements:
-            table[self.p1][self.p2] = empty_table[self.p1][self.p2]
-            table[p1][p2] = self.symbol
+    def move_piece(self, p1, p2):
+        if (p1 - self.p1, p2 - self.p2) in self.movements:
             self.p1 = p1
             self.p2 = p2
-            self.movements = [(p1 + 2, p2 + 1), (p1 - 2, p2 - 1), (p1 + 2, p2 - 1), (p1 - 2, p2 + 1),
-                            (p1 + 1, p2 + 2), (p1 - 1, p2 - 2), (p1 + 1, p2 - 2), (p1 - 1, p2 + 2)]
             self.moves+= 1
         else:
             print('El movimiento que ha insertado es invalido')
 
 class Tower(Piece):
-    symbols = ['♜','♖']
+    # symbols = ['♜','♖']
 
     def __init__(self, color, p1, p2):
         Piece.__init__(self, color, p1, p2)
-        self.symbol = self.symbols[self.color_p[color]]
-        self.movements = [(p1 + 1, p2), (p1, p2 + 1), (p1 - 1, p2), (p1, p2 - 1)]
+        # self.symbol = self.symbols[self.color_p[color]]
+        self.movements = [(1, 0), (0, 1), (-1, 0), (0, -1)]
     
-    def move_piece(self, table, p1, p2):
-        new_table = Table()
-        empty_table = new_table.create_table()
-        p_movements = []
-        for movement in self.movements:
-            if movement[0] < 8 and movement[0] >= 0:
-                if movement[1] < 8 and movement[1] >= 0:
-                    p_movements.append(movement)
-        if (p1, p2) in p_movements:
-            table[self.p1][self.p2] = empty_table[self.p1][self.p2]
-            table[p1][p2] = self.symbol
+    def move_piece(self, p1, p2):
+        if (p1 - self.p1, p2 - self.p2) in self.movements:
             self.p1 = p1
             self.p2 = p2
-            self.movements = [(p1 + 1, p2), (p1, p2 + 1), (p1 - 1, p2), (p1, p2 - 1)]
             self.moves+= 1
         else:
             print('El movimiento que ha insertado es invalido')
 
 class Pawn(Piece):
-    symbols = ['♟','♙']
+    # symbols = ['♟','♙']
 
     def __init__(self, color, p1, p2):
         Piece.__init__(self, color, p1, p2)
-        self.symbol = self.symbols[self.color_p[color]]
-        self.movements = [(p1 - 1, p2)]
-    
-    def move_piece(self, table, p1, p2):
-        p_movements = []
+        # self.symbol = self.symbols[self.color_p[color]]
+        self.movements = [(-1, 0), (-2, 0)]
         if self.color == 'black':
-            self.movements = [(self.p1 + 1, self.p2)]
-        for movement in self.movements:
-            if movement[0] < 8 and movement[0] >= 0:
-                if movement[1] < 8 and movement[1] >= 0:
-                    p_movements.append(movement)
-        if self.moves == 0 and self.color == 'white':
-            p_movements.append((self.p1 - 2, self.p2))
-        if self.moves == 0 and self.color == 'black':
-            p_movements.append((self.p1 + 2, self.p2))
-        if (p1, p2) in p_movements:
+            self.movements = [(1, 0), (2, 0)]
+    
+    def move_piece(self, p1, p2):
+        if (p1 - self.p1, p2 - self.p2) in self.movements:
             self.p1 = p1
             self.p2 = p2
-            self.movements = [(p1 - 1, p2)]
+            self.movements = [(-1, 0)]
+            if self.color == 'black':
+                self.movements = [(1, 0)]
             self.moves+= 1
         else:
             print('El movimiento que ha insertado es invalido')
+
+pawn = Pawn('white', 6, 4)
+
+print(pawn.p1, pawn.p2)
+
+pawn.move_piece(4, 4)
+
+print(pawn.p1, pawn.p2)
+
+pawn.move_piece(3, 4)
+
+print(pawn.p1, pawn.p2)
+
