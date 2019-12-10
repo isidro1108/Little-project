f74abde1a863f98d7a2c0a4518cf1976618029ee
isidro1108 <euris110811@gmail.com>
2019-12-10 10:17:18 -0400

    Se ha modicado la logica de la clase Pawn en cuanto a la direccion del peon

--- pieces/pawn.py
+++ pieces/pawn.py
@@ -2,13 +2,12 @@ from pieces import Piece
 
 class Pawn(Piece):
     # symbols = ['♟','♙']
+    address = {'white': -1, 'black': 1} # Direccion del peon dependiendo del color
 
     def __init__(self, color, p1, p2):
         Piece.__init__(self, color, p1, p2)
-        # self.symbol = self.symbols[self.color_p[color]]
-        self.movements = [(-1, 0), (-2, 0)]
-        if self.color == 'black':
-            self.movements = [(1, 0), (2, 0)]
+        self.dir = self.address[self.color] # Atributo que representa la direccion del peon
+        self.movements = [(1 * self.dir, 0), (2 * self.dir, 0)] # Primera aplicacion del atributo dir
 
     def __move_in_movements(self, p1, p2):
         return (p1 - self.p1, p2 - self.p2) in self.movements
@@ -17,9 +16,7 @@ class Pawn(Piece):
         if self.__move_in_movements(p1, p2):
             self.p1 = p1
             self.p2 = p2
-            self.movements = [(-1, 0)]
-            if self.color == 'black':
-                self.movements = [(1, 0)]
+            self.movements = [(1 * self.dir, 0)] # Segunda aplicacion del atributo dir
             self.moves+= 1
         else:
             print('El movimiento que ha insertado es invalido')
