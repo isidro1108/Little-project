0f8cba2a2f0a3e308d3c0fdf96605d6d64f238a0
Isidro Morla <euris110811@gmail.com>
2019-12-12 23:49:46 -0400

    Se ha creado el método capture en la clase Piece

--- father_class/piece.py
+++ father_class/piece.py
@@ -18,3 +18,16 @@ class Piece:
             self.moves+= 1
         else:
             print('El movimiento que ha insertado es invalido')
+
+    def capture(self, table, p1, p2):
+        if self.move_in_movements(p1, p2) and table.is_piece(p1, p2):
+            f_piece = table.c_table[p1][p2]
+            is_enemy_piece = isinstance(f_piece, Piece) and f_piece.color != self.color
+            if is_enemy_piece:
+                table.c_table[p1][p2], table.c_table[self.p1][self.p2] = self, f_piece.my_box
+                self.p1, self.p2 = p1, p2
+                self.moves+= 1
+            else:
+                print('Esta no es una pieza enemiga')
+        else:
+            print('Este no es un movimiento válido o no hay pieza para capturar')
