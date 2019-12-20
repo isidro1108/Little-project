96ac8ba498568bdeb89529f0eed161e6f059db95
Isidro Morla <euris110811@gmail.com>
2019-12-19 23:02:42 -0400

    Revert "Se ha agregado el atributo can_do_castling en King y en Tower"
    This reverts commit f6406e6fb9dbe823c4497cb30ea7bd9d7bb3dd09.

--- pieces/king.py
+++ pieces/king.py
@@ -7,6 +7,5 @@ class King(Piece):
         Piece.__init__(self, color, p1, p2)
         self.in_check = False
         self.in_checkmate = False
-        self.can_do_castling = True
         self.movements = [(1, 1), (-1, -1), (1, -1), (-1, 1),
                         (1, 0), (-1, 0), (0, 1), (0, -1)]
--- pieces/tower.py
+++ pieces/tower.py
@@ -5,5 +5,4 @@ class Tower(Piece):
 
     def __init__(self, color, p1, p2):
         Piece.__init__(self, color, p1, p2)
-        self.can_do_castling = True
         self.movements = [(1, 0), (0, 1), (-1, 0), (0, -1)]
