d7f68566ebb7c2b6b40050628039c186c1c18eca
Isidro Morla <euris110811@gmail.com>
2020-02-06 09:45:07 -0600

    Se ha eliminado el atributo in_checkmate de king

--- pieces/king.py
+++ pieces/king.py
@@ -4,7 +4,6 @@ class King(Piece):
     def __init__(self, color, p1, p2):
         Piece.__init__(self, color, p1, p2)
         self.in_check = False
-        self.in_checkmate = False
         self.movements = [(1, 1), (-1, -1), (1, -1), (-1, 1),
                         (1, 0), (-1, 0), (0, 1), (0, -1)]
     
