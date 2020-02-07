488ac923ba762e976c75dcf7401e248be082d4fd
Isidro Morla <euris110811@gmail.com>
2020-02-07 13:51:05 -0600

    El programa funciona con cualquier entrada en la consola

--- game.py
+++ game.py
@@ -12,29 +12,42 @@ player2.insert_pieces(chess_table)
 
 col = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
 
+def valid_position(p):
+    positions = []
+    for c in range(ord('a'), ord('h') + 1):
+        for r in range(1, 9):
+            positions.append(chr(c) + str(r))
+    return p in positions
+
 def move(player):
     p = input('Inserte la posicion de la pieza que desea mover: ')
     pd = input('Donde desea moverse: ')
-    p1, p2 = 8 - int(p[1]), col[p[0]]
-    pd1, pd2 = 8 - int(pd[1]), col[pd[0]]
-    move = player.move(chess_table, p1, p2, pd1, pd2)
-    return move
+    if valid_position(p) and valid_position(pd):
+        p1, p2 = 8 - int(p[1]), col[p[0]]
+        pd1, pd2 = 8 - int(pd[1]), col[pd[0]]
+        move = player.move(chess_table, p1, p2, pd1, pd2)
+        return move
+    return False
 
 def capture(player):
     p = input('Inserte la posicion de la pieza con la que desea capturar: ')
     pd = input('Donde desea capturar: ')
-    p1, p2 = 8 - int(p[1]), col[p[0]]
-    pd1, pd2 = 8 - int(pd[1]), col[pd[0]]
-    move = player.capture(chess_table, p1, p2, pd1, pd2)
-    return move
+    if valid_position(p) and valid_position(pd):
+        p1, p2 = 8 - int(p[1]), col[p[0]]
+        pd1, pd2 = 8 - int(pd[1]), col[pd[0]]
+        move = player.capture(chess_table, p1, p2, pd1, pd2)
+        return move
+    return False
 
 def step_capture(player):
     p = input('Inserte la posicion de la pieza con la que desea capturar: ')
     pd = input('Donde desea capturar: ')
-    p1, p2 = 8 - int(p[1]), col[p[0]]
-    pd1, pd2 = 8 - int(pd[1]), col[pd[0]]
-    move = player.step_capture(chess_table, p1, p2, pd1, pd2)
-    return move
+    if valid_position(p) and valid_position(pd):
+        p1, p2 = 8 - int(p[1]), col[p[0]]
+        pd1, pd2 = 8 - int(pd[1]), col[pd[0]]
+        move = player.step_capture(chess_table, p1, p2, pd1, pd2)
+        return move
+    return False
 
 def castling_to_left(player):
     move = player.castling_to_left(chess_table)
@@ -96,21 +109,23 @@ while True:
     print_chess_table()
     print()
     print('1. Mover  2. Capturar  3. Capturar al paso  4. Enroque a la izquierda  5. Enroque a la derecha')
-    op = int(input('Elige una opcion: '))
+    op = input('Elige una opcion: ')
     print(op)
-    if op == 1:
+    if op == '1':
         if move(player):
             turn+= 1
-    elif op == 2:
+    elif op == '2':
         if capture(player):
             turn+= 1
-    elif op == 3:
+    elif op == '3':
         if step_capture(player):
             turn+= 1
-    elif op == 4:
+    elif op == '4':
         if castling_to_left(player):
             turn+= 1
-    elif op == 5:
+    elif op == '5':
         if castling_to_right(player):
             turn+= 1
+    else:
+        print('Elija la opcion correctamente')
     
